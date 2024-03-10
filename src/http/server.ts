55cac574e188b7defa52baccfe4b1b643ad579a0
Jose Lima <jalimag0206@gmail.com>
2024-03-10 17:32:43 -0300

    feat: add create poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,8 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
 
 import { createPoll } from './routes/create-poll'
 
 const app = fastify()
-
-const prisma = new PrismaClient()
 
 app.register(createPoll)
 
