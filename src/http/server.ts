d6573371fcc1248834a033dc99c538850f570d5a
Jose Lima <jalimag0206@gmail.com>
2024-03-12 08:57:36 -0300

    feat: add voteOnPool route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
-import { createPoll, getPoll } from './routes/'
+import { createPoll, getPoll, voteOnPoll } from './routes/'
 
 const app = fastify()
 
+app.register(cookie, {
+  secret: 'polls-secret-app',
+  hook: 'onRequest',
+  parseOptions: {},
+})
+
 app.register(getPoll)
 app.register(createPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server Running!')
